260f6f8d165e30751afc2d2aa4d41f77b55528d6
Diego <diego_alessandro@hotmail.com>
2022-03-30 12:54:25 -0300

    Segundo commit

--- class.py
+++ class.py
@@ -1,6 +1,6 @@
 from numbers import Number
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, VARCHAR, CHAR, NUMERIC, Date
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy_utils import create_database, database_exists
 from sqlalchemy.ext.declarative import declarative_base
 from local_settings import postgresql as settings
@@ -33,6 +33,7 @@ def get_session():
     session = sessionmaker(bind=engine) ()
     return session
 
+engine = get_engine_from_settings()
 session = get_session()
 session
 
@@ -43,86 +44,95 @@ Base = declarative_base()
 class Artistas(Base):
     __tablename__= 'artistas'
 
-    cod_art = Column(Number(4), primary_key=True)
+    cod_art = Column(NUMERIC(4), primary_key=True)
     tpo_art = Column(CHAR(1))
     nac_bras = Column(CHAR(1))
-    cod_grav = Column(Number(4), ForeignKey("gravadoras.cod_grav"))
-    qtd_tit = Column(Number(4))
-    med_anual = Column(Number(4,2))
+    cod_grav = Column(NUMERIC(4), ForeignKey("gravadoras.cod_grav"))
+    qtd_tit = Column(NUMERIC(4))
+    med_anual = Column(NUMERIC(4,2))
     nom_art = Column(VARCHAR(40))
+    gravadora = relationship('Gravadoras')
 
 
 class Copias(Base):
     __tablename__= 'copias'
 
-    cod_tit = Column(Number(6), ForeignKey("titulos.cod_tit"), primary_key=True )
-    num_cop = Column(Number(2), primary_key=True)
+    cod_tit = Column(NUMERIC(6), ForeignKey("titulos.cod_tit"), primary_key=True )
+    num_cop = Column(NUMERIC(2), primary_key=True)
     dat_aq = Column(Date)
     status = Column(CHAR(1))
+    titulos = relationship('titulos')
 
 
 class Gravadoras(Base):
     __tablename__= 'gravadoras'
 
-    cod_grav= Column(Number(4), primary_key=True)
+    cod_grav= Column(NUMERIC(4), primary_key=True)
     uf_grav = Column(CHAR(2))
     nac_bras = Column(CHAR(1))
     nom_grav = Column(VARCHAR(40))
+    artistas = relationship(Artistas, backref='gravadoras')
 
 
 class Itens_Locacoes(Base):
     __tablename__= 'itens_locacoes'
 
-    cod_soc = Column(Number(4),ForeignKey("locacoes.cod_soc"), primary_key=True)
-    dat_loc = Column(Date, ForeignKey("locacoes.dat_loc"), primary_key=True)
-    cod_tit = Column(Number(6), ForeignKey("copias.cod_tit"), primary_key=True)
-    num_cop = Column(Number(2), ForeignKey("copias.num_cop"))
+    cod_soc = Column(NUMERIC(4),ForeignKey("locacoes.cod_soc"), primary_key=True)
+    dat_loc = Column(Date,ForeignKey("locacoes.dat_loc"), primary_key=True)
+    cod_tit = Column(NUMERIC(6), ForeignKey("copias.cod_tit"), primary_key=True)
+    num_cop = Column(NUMERIC(2), ForeignKey("copias.num_cop"))
     dat_prev = Column(Date)
-    val_loc = Column(Number(6,2))
+    val_loc = Column(NUMERIC(6,2))
     sta_mul = Column(CHAR(1))
     dat_dev = Column(Date)
+    locacoes_soc= relationship('locacoes')
 
 
 class Locacoes(Base):
     __tablename__= 'locacoes'
 
-    cod_soc = Column(Number(4), ForeignKey("socios.cod_soc"), primary_key=True)
+    cod_soc = Column(NUMERIC(4), ForeignKey("socios.cod_soc"), primary_key=True)
     dat_loc = Column(Date, primary_key=True)
-    val_loc = Column(Number(5,2))
+    val_loc = Column(NUMERIC(5,2))
     dat_venc = Column(Date)
     sta_pgto = Column(CHAR(1))
     dat_pgto = Column(Date)
+    locacoes = relationship(Itens_Locacoes, backref= 'locacoes')
 
 
 class Socios(Base):
     __tablename__= 'socios'
 
-    cod_soc = Column(Number(4), primary_key=True)
+    cod_soc = Column(NUMERIC(4), primary_key=True)
     dat_cad = Column(Date)
-    cod_tps = Column(Number(4), ForeignKey("tipos_socios.cod_tps"))
+    cod_tps = Column(NUMERIC(4), ForeignKey("tipos_socios.cod_tps"))
     sta_soc = Column(CHAR(1))
     nom_soc = Column(VARCHAR(40))
+    tipos_socio= relationship('tipos_socios')
 
 
 class Tipos_Socios(Base):
     __tablename__ = 'tipos_socios'
 
-    cod_tps = Column(Number(4), primary_key=True)
-    lim_tit = Column(Number(2))
-    val_base = Column(Number(6,2))
+    cod_tps = Column(NUMERIC(4), primary_key=True)
+    lim_tit = Column(NUMERIC(2))
+    val_base = Column(NUMERIC(6,2))
     dsc_tps = Column(VARCHAR(40))
+    socios = relationship(Socios, backref= 'tipos_socios')
 
 
 class Titulos(Base):
     __tablename__ = 'titulos'
 
-    cod_tit = Column(Number(6), primary_key=True)
+    cod_tit = Column(NUMERIC(6), primary_key=True)
     tpo_tit = Column(CHAR(1))
     cla_tit = Column(CHAR(1))
-    qtd_cop = Column(Number(3))
+    qtd_cop = Column(NUMERIC(3))
     dat_lanc = Column(Date)
-    cod_art = Column(Number(4), ForeignKey("artistas.cod_art"))
-    cod_grav = Column(Number(4), ForeignKey("artistas.cod_grav"))
+    cod_art = Column(NUMERIC(4), ForeignKey("artistas.cod_art"))
+    cod_grav = Column(NUMERIC(4), ForeignKey("gravadoras.cod_grav"))
     dsc_tit = Column(VARCHAR(40))
+    copias = relationship(Copias, backref='titulos')
 
 
+Base.metadata.create_all(engine)
